2e0264e0b260abe1fb3563bbf8eb396ecb885691
Daniel Spady <danielspadyassurance22@gmail.com>
2026-09-09 10:07:46 -0400

    Initial Commit

---
+++ CVSRickMorty/CVSRickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSRickMortyApp.swift
+//  CVSRickMorty
+//
+//  Created by Daniel Spady on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CVSRickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVSRickMortyTests/CVSRickMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CVSRickMortyTests.swift
+//  CVSRickMortyTests
+//
+//  Created by Daniel Spady on 9/9/26.
+//
+
+import Testing
+@testable import CVSRickMorty
+
+struct CVSRickMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
